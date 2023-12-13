cdbd2ef249fc4d81c8029e2656c1ddef22630432
ekoyudop <ekoyudap2@gmail.com>
2023-12-13 20:21:39 +0700

    flask index

--- app.py
+++ app.py
@@ -17,5 +17,21 @@ db = client[DB_NAME]
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/discover')
+def discover():
+    return render_template('discover.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
