80da6d89b84f88b2df93b1682ddf9665ce9ce43f
ekoyudop <ekoyudap2@gmail.com>
2023-12-13 19:51:13 +0700

    flask

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from pymongo import MongoClient
+import requests
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -10,9 +12,10 @@ load_dotenv(dotenv_path)
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
-
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
 
-
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
