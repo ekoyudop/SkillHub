6d007500efa0b4df1dde25b58d642456b4c31b8e
ekoyudop <ekoyudap2@gmail.com>
2023-12-14 17:01:57 +0700

    update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
+import jwt
+import datetime
+import hashlib
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -17,6 +20,8 @@ db = client[DB_NAME]
 
 app = Flask(__name__)
 
+SECRET_KEY = "SKILLHUB"
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -29,9 +34,22 @@ def discover():
 def register():
     return render_template('register.html')
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    msg = request.args.get("msg")
+    return render_template("login.html", msg=msg)
+
+@app.route('/home_visitor')
+def login():
+    token_receive = request.cookies.get("mytoken")
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=["HS256"])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template("home_visitor.html", user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="Your token has expired"))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="There was problem logging you in"))
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
